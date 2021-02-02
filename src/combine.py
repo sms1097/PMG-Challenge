fd1d7843976af3b3dde2e699a07bdb26d103ca16
Sean Smith <sms1097@wildcats.unh.edu>
2021-02-01 22:08:02 -0500

    Realization that I did not write to stdout

--- src/combine.py
+++ src/combine.py
@@ -11,21 +11,20 @@ def combine(paths):
             paths (list): string for path to file
         Return: None
     """
-    with open(paths[-1], 'w') as out:
-        # init writer and create header
-        writer = csv.writer(out, delimiter=',')
-        writer.writerow(['email_hash', 'category', 'filename'])
+    # init writer and create header
+    writer = csv.writer(sys.stdout)
+    writer.writerow(['email_hash', 'category', 'filename'])
 
-        # iterate through files to write
-        for file in paths[:-1]:
-            with open(file, 'r') as f:
-                reader = csv.reader(f)
-                reader.__next__()
+    # iterate through files to write
+    for file in paths:
+        with open(file, 'r') as f:
+            reader = csv.reader(f)
+            reader.__next__()
 
-                filename = ntpath.split(file)[-1]
-                for line in reader:
-                    line.append(filename)
-                    writer.writerow(line)
+            filename = ntpath.split(file)[-1]
+            for line in reader:
+                line.append(filename)
+                writer.writerow(line)
 
 
 if __name__ == '__main__':
