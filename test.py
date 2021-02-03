401543f1244ce441729d5500cbdd80615afeec70
Sean Smith <sms1097@wildcats.unh.edu>
2021-02-03 15:54:37 -0500

    Added more tests

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import pandas as pd
+import sys
 import unittest
+from io import StringIO
+from src.combine import combine
+import pandas as pd
 
 
 class CombineTests(unittest.TestCase):
@@ -11,6 +14,16 @@ class CombineTests(unittest.TestCase):
 
         self.assertTrue(basic.equals(inm))
         self.assertTrue(inm.equals(oom))
+
+    def test_static_file1(self):
+        result = pd.read_csv('model_outputs/stat_test1.csv')
+        test1 = pd.read_csv('test_files/test1.csv')
+        self.assertTrue(test1.equals(result))
+
+    def test_static_file2(self):
+        result = pd.read_csv('model_outputs/stat_test2.csv')
+        test1 = pd.read_csv('test_files/test2.csv')
+        self.assertTrue(test1.equals(result))
 
 
 if __name__ == '__main__':
